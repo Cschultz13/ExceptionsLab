7bad93b7d47e5dc7da21514b25d689a03e482e77
Carson Schultz <Carson Schultz@10.30.20.132>
2016-10-13 15:16:47 -0500

    fixed lab 1 bug and lab 0 typos

--- src/lab0/Employee.java
+++ src/lab0/Employee.java
@@ -1,22 +1,23 @@
 package lab0;
 
 /**
- * In this challenge you need to address the probability that at some
- * point the arguments to method parameters may not be valid. 
+ * In this challenge you need to address the probability that at some point the
+ * arguments to method parameters may not be valid.
  * <p>
- * For example, String arguments may be null or empty; other objects may be 
+ * For example, String arguments may be null or empty; other objects may be
  * null; or primitive numbers may be out of acceptable range.
  * <p>
- * You need to validate ALL method parameters to make sure any and all 
- * arguments are valid. The only exception is when any argument is acceptable 
- * based on requirements. Fix the code below using if logic to validate
- * method arguments and throw IllegalArgumentException or a custom
- * exception if the validation fails.
- * 
- * @author  Jim Lombardo, jlombardo@wctc.edu
+ * You need to validate ALL method parameters to make sure any and all arguments
+ * are valid. The only exception is when any argument is acceptable based on
+ * requirements. Fix the code below using if logic to validate method arguments
+ * and throw IllegalArgumentException or a custom exception if the validation
+ * fails.
+ *
+ * @author Jim Lombardo, jlombardo@wctc.edu
  * @version 1.00
  */
 public class Employee {
+
     public static final int MAX_VACATION_DAYS = 28;
     private String firstName;
     private String lastName;
@@ -25,7 +26,6 @@ public class Employee {
     private static final int MAX_NAME_LENGTH = 30;
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MIN_VACATION_DAYS = 0;
-    
 
     public Employee() {
         // initialize a bunch of default values
@@ -41,15 +41,16 @@ public class Employee {
         setSsn(ssn);
         this.daysVacation = daysVacation;
     }
-    
+
     public int getDaysVacation() {
         return daysVacation;
     }
 
-    public void setDaysVacation(int daysVacation) {
-        if(daysVacation < MIN_VACATION_DAYS ||daysVacation > MAX_VACATION_DAYS){
+    public void setDaysVacation(int daysVacation) throws IllegalArgumentException {
+        if (daysVacation < MIN_VACATION_DAYS || daysVacation > MAX_VACATION_DAYS) {
             throw new IllegalArgumentException("Sorry the entered vacation time is invalid");
         }
+
         this.daysVacation = daysVacation;
     }
 
@@ -57,10 +58,11 @@ public class Employee {
         return firstName;
     }
 
-    public final void setFirstName(String firstName) {
-       if(firstName.length() < MIN_NAME_LENGTH|| firstName.length() > MAX_NAME_LENGTH || firstName == null){
-           throw new IllegalArgumentException("Sorry the entered name length is invalid");
-       }
+    public final void setFirstName(String firstName) throws IllegalArgumentException {
+        if (firstName.length() < MIN_NAME_LENGTH || firstName.length() > MAX_NAME_LENGTH || firstName == null) {
+            throw new IllegalArgumentException("Sorry the entered name length is invalid");
+
+        }
         this.firstName = firstName;
     }
 
@@ -68,10 +70,11 @@ public class Employee {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        if(lastName.length() < MIN_NAME_LENGTH|| lastName.length() > MAX_NAME_LENGTH || firstName == null){
-           throw new IllegalArgumentException("Sorry the entered name length is invalid");
-       }
+    public void setLastName(String lastName) throws IllegalArgumentException {
+        if (lastName.length() < MIN_NAME_LENGTH || lastName.length() > MAX_NAME_LENGTH || lastName == null) {
+            throw new IllegalArgumentException("Sorry the entered name length is invalid");
+
+        }
         this.lastName = lastName;
     }
 
@@ -83,7 +86,5 @@ public class Employee {
         // validation depends on requirements
         this.ssn = ssn;
     }
-    
-    
-    
+
 }
--- src/lab0/Startup.java
+++ src/lab0/Startup.java
@@ -7,13 +7,15 @@ package lab0;
 public class Startup {
 
     public static void main(String[] args) {
-
-        Employee emp = new Employee("Jim","Smith","3333333333",28);
-        
-        // Think about this code. It wouldn't work if lastName was null
-        String fullNameInCaps = 
-                emp.getFirstName().toUpperCase() 
-                + " " + emp.getLastName().toUpperCase();
-        
+        String fullNameInCaps = "null";
+        try {
+            Employee emp = new Employee("Jim", "Smith", "3333333333", 28);
+            fullNameInCaps
+                    = emp.getFirstName().toUpperCase()
+                    + " " + emp.getLastName().toUpperCase();
+        } catch (IllegalArgumentException ia) {
+            System.out.println(ia.getMessage());
+        }
+        System.out.println(fullNameInCaps);
     }
 }
